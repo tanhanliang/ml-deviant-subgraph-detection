06e5bf30855a998b87dab28559e99a317e1e3c05
tanhanliang <tanhanliang@github.com>
2017-12-16 10:16:06 +0800

    Test for build_adjacency_matrix

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -7,34 +7,69 @@ from data_processing.preprocessing import *
 
 
 class MockNode:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, node_id, properties=None):
+        self.id = node_id
+
+        if properties is None:
+            self.properties = {}
+        else:
+            self.properties = properties
 
 
 class MockEdge:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, edge_id, start=-1, end=-1, edge_type='NoType'):
+        self.id = edge_id
+        self.start = start
+        self.end = end
+        self.type = edge_type
 
 
 class MockPath:
-    def __init__(self):
-        self.nodes = [MockNode(1), MockNode(2), MockNode(3)]
-        self.relationships = [MockEdge(1), MockEdge(2), MockEdge(3)]
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.relationships = edges
 
 
 class MockBoltStatementResult:
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
+
     def data(self):
-        return [{'path': MockPath()}]
+        return [{'path': MockPath(self.nodes, self.edges)}]
 
 
 class TestPreprocessingFns(unittest.TestCase):
     def test_get_nodes_edges(self):
-        data = MockBoltStatementResult()
+        nodes = [MockNode(1), MockNode(2), MockNode(3)]
+        edges = [MockEdge(1), MockEdge(2), MockEdge(3)]
+        data = MockBoltStatementResult(nodes, edges)
         nodes, edges = get_nodes_edges(data)
 
-        for id in range(1,4):
-            self.assertEquals(id, nodes[id].id)
-            self.assertEquals(id, edges[id].id)
+        for node_id in range(1, 4):
+            self.assertEquals(id, nodes[node_id].id)
+            self.assertEquals(id, edges[node_id].id)
+
+    def test_build_adjacency_matrix(self):
+        nodes = [MockNode(1), MockNode(2), MockNode(3), MockNode(4)]
+        edges = [MockEdge(1, 1, 2), MockEdge(2, 2, 3), MockEdge(3, 3, 4)]
+        data = MockBoltStatementResult(nodes, edges)
+        adj_matrix = build_adjacency_matrix(data)
+        shape = adj_matrix.matrix.shape
+
+        self.assertEquals(shape, (len(nodes), len(nodes)))
+
+        index_to_id = {}
+        for node_id, index in adj_matrix.id_to_index.items():
+            index_to_id[index] = node_id
+
+        for row in range(shape[0]):
+            for col in range(shape[1]):
+                if adj_matrix.matrix[row, col] == 1:
+                    start_id = index_to_id[row]
+                    end_id = index_to_id[col]
+                    self.assertTrue(end_id-start_id == 1)
+
 
 
 def main():
