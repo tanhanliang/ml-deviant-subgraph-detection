fdcaa6993ff074cccea0eec56744740c3bc0697e
tanhanliang <tanhanliang@github.com>
2017-12-20 17:52:42 +0800

    Fixed function to sort nodes

--- data_processing/neighborhood_assembly.py
+++ data_processing/neighborhood_assembly.py
@@ -5,49 +5,35 @@ algorithm.
 import queue
 
 
-def generate_node_list(node_cmp_fn, nodes):
+def generate_node_list(transform_fn, nodes):
     """
-    Given a function used to order nodes and a Dictionary of node_id -> node which represents all
-    nodes in the graph, returns a list of nodes ordered by the labeling function.
+    Sorts a list of nodes by some labeling function, for example sorting by node timestamp.
 
-    The node ordering function compares two nodes and returns:
-    (1) -1 if the first node is ordered before the second node,
-    (2) 0 if the nodes are equivalent in order, and
-    (3) 1 if the first node is ordered after the second node.
-
-    :param node_cmp_fn: A function which takes two nodes as arguments and returns an integer
+    :param transform_fn: A function which takes a node as an argument and returns some value
+    to be used for sorting
     :param nodes: A Dictionary of node_id -> node
-    :return: A list of nodes in the correct order
+    :return: A list of sorted nodes
     """
 
     nodes_list = list(nodes.values())
-    sorted(nodes_list, cmp=node_cmp_fn)
+    nodes_list = sorted(nodes_list, key=transform_fn)
     return nodes_list
 
 
-def ts_ordering_asc(node1, node2):
+def ts_ordering(node):
     """
-    Generates an integer representing the relative ordering of the nodes based on timestamp.
-    If node1's timestamp is smaller than node2's timestamp, then node1 is ordered before node2,
-    and vice versa. This function assumes that a timestamp exists in the properties dict of the
-    node, otherwise it will throw an exception.
+    Given a node, returns its timestamp if it exists, otherwise throws a RuntimeError.
+    This fn will be used to sort a list of nodes by timestamp, using the built in sorted()
+    function.
 
-    :param node1: First node to compare
-    :param node2: Second node to compare
-    :return: An integer representing the relative ordering of node1 compared to node2
+    :param node: A node in a list to be sorted
+    :return: The timestamp of the node
     """
 
-    if 'timestamp' not in node1.properties or 'timestamp' not in node2.properties:
+    if 'timestamp' not in node.properties:
         raise RuntimeError('timestamp does not exist in properties dict of node')
-    node1_ts = node1.properties['timestamp']
-    node2_ts = node2.properties['timestamp']
 
-    if node1_ts < node2_ts:
-        return -1
-    elif node1_ts == node2_ts:
-        return 0
-    else:
-        return 1
+    return node.properties['timestamp']
 
 
 def get_receptive_field(root_id, nodes, outgoing_edges, size):
