105d65db8bb6d8a7caf783b138b276916ca22003
tanhanliang <tanhanliang@github.com>
2017-12-25 16:08:50 +0800

    Print paths visited by attack nodes arranged by freq

--- utility/db_exploration.py
+++ utility/db_exploration.py
@@ -57,3 +57,30 @@ def get_attack_paths(attack_nodes):
         path_sets.append(paths)
     session.close()
     return path_sets
+
+
+def get_path_counts(path_sets):
+    """
+    Counts how many times each path is visited by all the attack nodes.
+
+    :param path_sets: A list of sets of paths
+    :return: A Dictionary of path (String) -> count
+    """
+
+    counts = {}
+
+    for set in path_sets:
+        for path in set:
+            if path not in counts.keys():
+                counts[path] = 0
+            counts[path] += 1
+
+    return counts
+
+
+def print_paths_by_freq(counts):
+    items = counts.items()
+    items = sorted(items, key=lambda x: x[1])
+
+    for item in items:
+        print(item)
