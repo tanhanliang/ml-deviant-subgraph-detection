0b321b4b3f767157a6025f7be0d79ba4cd37ff00
tanhanliang <tanhanliang@github.com>
2018-03-01 21:27:31 +0000

    Updated load data function for latest data

--- utility/load_data.py
+++ utility/load_data.py
@@ -6,17 +6,19 @@ import numpy as np
 
 def load_data():
     """
-    Loads all data available.
+    Loads data for the latest version of the model.
 
     :return: A tuple of ndarrays, (x,y).
     x has shape (training_examples, field_size*field_count, channel_count, 1)
     y has shape (training_examples, class_count)
     """
 
-    x_1 = np.fromfile("training_data/x_train1.txt")
+    x_embed = np.fromfile("training_data/x_embed4.txt")
+    x_patchy = np.fromfile("training_data/x_patchy4.txt")
+    y = np.fromfile("training_data/y_train4.txt")
     # Shape is currently hardcoded until i find a better way to do it
-    x_1 = x_1.reshape((378, 3, 5, 1))
-    y_1 = np.fromfile("training_data/y_train1.txt")
-    y_1 = y_1.reshape((378, 2))
+    x_embed = x_embed.reshape((2000, 40))
+    x_patchy = x_patchy.reshape((2000, 4, 5, 1))
+    y = y.reshape((2000, 2))
 
-    return x_1, y_1
+    return x_patchy, x_embed, y
