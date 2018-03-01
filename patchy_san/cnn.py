a9f16f618e1a77c76be75e1e72f114aec50df8a6
tanhanliang <tanhanliang@github.com>
2018-03-01 21:01:15 +0000

    Added names to model layers

--- patchy_san/cnn.py
+++ patchy_san/cnn.py
@@ -21,28 +21,35 @@ def build_model(learning_rate=0.005, activations="sigmoid"):
     """
     patchy_san_input_shape = (FIELD_COUNT*MAX_FIELD_SIZE, CHANNEL_COUNT, 1)
 
-    patchy_san_input = Input(shape=patchy_san_input_shape)
-    conv = Convolution2D(
-            activation='relu',
-            filters=8,
-            kernel_size=(1, 2),
-            input_shape=patchy_san_input_shape)(patchy_san_input)
-    maxpool = MaxPooling2D(pool_size=(1, 2))(conv)
-    dropout1 = Dropout(0.1)(maxpool)
-    flatten1 = Flatten()(dropout1)
+    # Patchy-san track
+    patchy_san_input = Input(shape=patchy_san_input_shape, name='ps_input')
+    ps_conv1 = Convolution2D(
+        activation='relu',
+        filters=8,
+        kernel_size=(1, 2),
+        input_shape=patchy_san_input_shape,
+        name='ps_conv1'
+    )(patchy_san_input)
+    ps_maxpool1 = MaxPooling2D(pool_size=(1, 2), name='ps_maxpool1')(ps_conv1)
+    ps_dropout1 = Dropout(0.1, name='ps_dropout1')(ps_maxpool1)
+    ps_flatten1 = Flatten(name='ps_flatten1')(ps_dropout1)
 
-    embedding_input = Input(shape=(EMBEDDING_LENGTH*MAX_NODES,))
-    embedding = Embedding(VOCAB_SIZE,
-                          EMBEDDING_DIM,
-                          input_length=EMBEDDING_LENGTH*MAX_NODES)(embedding_input)
-    flatten2 = Flatten()(embedding)
+    # Embedding track
+    emb_input = Input(shape=(EMBEDDING_LENGTH*MAX_NODES,), name='emb_input')
+    emb_embedding = Embedding(
+        VOCAB_SIZE,
+        EMBEDDING_DIM,
+        input_length=EMBEDDING_LENGTH*MAX_NODES,
+        name='emb_embedding'
+    )(emb_input)
+    emb_flatten = Flatten(name='emb_flatten')(emb_embedding)
 
-    merge = concatenate([flatten1, flatten2])
-    dense1 = Dense(8, activation='relu')(merge)
-    dense2 = Dense(8, activation='relu')(dense1)
-    dropout2 = Dropout(0.1)(dense2)
-    output = Dense(CLASS_COUNT, activation=activations)(dropout2)
-    model = Model(inputs=[patchy_san_input, embedding_input], outputs=output)
+    merge = concatenate([ps_flatten1, emb_flatten], name='merge')
+    dense1 = Dense(8, activation='relu', name='dense1')(merge)
+    dense2 = Dense(8, activation='relu', name='dense2')(dense1)
+    dropout1 = Dropout(0.1, name='dropout1')(dense2)
+    output = Dense(CLASS_COUNT, activation=activations, name='output')(dropout1)
+    model = Model(inputs=[patchy_san_input, emb_input], outputs=output)
     optimiser = adam(lr=learning_rate)
     model.compile(loss='mean_squared_error',
                   optimizer=optimiser,
