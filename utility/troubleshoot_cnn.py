edc63a0a9b02b3de6ca73da2f836890681cd55bc
tanhanliang <tanhanliang@github.com>
2018-03-01 20:43:22 +0000

    View intermediate layer outputs using keras model instead of keras function

--- utility/troubleshoot_cnn.py
+++ utility/troubleshoot_cnn.py
@@ -2,30 +2,19 @@
 Contains functions to help me debug the cnn.
 """
 
-import keras.backend as backend
+from keras.models import Model
 from matplotlib import pyplot as plt
 
 
-def get_nth_layer_output_fn(model, n):
+def get_nth_layer_output_model(model, layer_name):
     """
-    Returns a Keras function to return the output of a certain layer. Similar to the
-    original Keras model, this function takes a tuple of numpy ndarrays as input:
-    (x,y). x is a 4D numpy array of training examples, y is a 2D numpy array of
-    target values per training example using one-hot encoding.
+    Creates a Model which outputs the output of the desired layer
 
-    To use the resulting function:
-
-    layer3_output = get_nth_layer_output_fn(model, 3)
-    output = layer3_output([x_val, learning_phase_flag])
-
-    learning_phase_flag is 0 for test mode, 1 for training mode.
-
-    :param model: The Keras model
-    :param n: The layer that you want output from
-    :return: Keras function that will return the output of a certain layer given a certain input
+    :param model: A keras Model
+    :param layer_name: The name of the layer
+    :return: A keras Model
     """
-
-    return backend.function([model.layers[0].input, backend.learning_phase()], [model.layers[n].output])
+    return Model(inputs=model.input, outputs=model.get_layer(layer_name).output)
 
 
 def plot_eval_metrics(history):
