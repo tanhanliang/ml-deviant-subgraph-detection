d8ff51abdd924dcf7de5cca3ce725ff0b733044f
tanhanliang <tanhanliang@github.com>
2018-03-14 23:27:49 +0000

    Added old 2-input model for evaluation

--- patchy_san/cnn.py
+++ patchy_san/cnn.py
@@ -17,7 +17,7 @@ def build_model(learning_rate=0.005, activations="sigmoid"):
     Currently only data from node properties is considered, but edge data will be
     incorporated into the model later.
 
-    :return:
+    :return: A keras Model
     """
     ps_nodes_input_shape = (FIELD_COUNT*MAX_FIELD_SIZE, CHANNEL_COUNT, 1)
     ps_edges_input_shape = (FIELD_COUNT*MAX_NODES*MAX_NODES, 2, 1)
@@ -65,6 +65,54 @@ def build_model(learning_rate=0.005, activations="sigmoid"):
     dropout1 = Dropout(0.1, name='dropout1')(dense2)
     output = Dense(CLASS_COUNT, activation=activations, name='output')(dropout1)
     model = Model(inputs=[ps_nodes_input, ps_edges_input, emb_input], outputs=output)
+    optimiser = adam(lr=learning_rate)
+    model.compile(loss='mean_squared_error',
+                  optimizer=optimiser,
+                  metrics=['accuracy'])
+    return model
+
+
+def build_double_input_model(learning_rate=0.005, activations="sigmoid"):
+    """
+    Builds the model which only implemented patchy-san for nodes and word embeddings.
+    Also for testing purposes.
+
+    :param learning_rate:
+    :param activations:
+    :return: A keras Model
+    """
+
+    ps_nodes_input_shape = (FIELD_COUNT*MAX_FIELD_SIZE, CHANNEL_COUNT, 1)
+
+    # Patchy-san nodes track
+    ps_nodes_input = Input(shape=ps_nodes_input_shape, name='ps_nodes_input')
+    psn_conv1 = Convolution2D(
+        activation='relu',
+        filters=8,
+        kernel_size=(1, 2),
+        input_shape=ps_nodes_input_shape,
+        name='ps_nodes_conv1'
+    )(ps_nodes_input)
+    psn_maxpool1 = MaxPooling2D(pool_size=(1, 2), name='ps_nodes_maxpool1')(psn_conv1)
+    psn_dropout1 = Dropout(0.1, name='ps_nodes_dropout1')(psn_maxpool1)
+    psn_flatten1 = Flatten(name='ps_nodes_flatten1')(psn_dropout1)
+
+    # Embedding track
+    emb_input = Input(shape=(EMBEDDING_LENGTH*MAX_NODES*2,), name='emb_input')
+    emb_embedding = Embedding(
+        VOCAB_SIZE,
+        EMBEDDING_DIM,
+        input_length=EMBEDDING_LENGTH*MAX_NODES*2,
+        name='emb_embedding'
+    )(emb_input)
+    emb_flatten = Flatten(name='emb_flatten')(emb_embedding)
+
+    merge = concatenate([psn_flatten1, emb_flatten], name='merge')
+    dense1 = Dense(8, activation='relu', name='dense1')(merge)
+    dense2 = Dense(8, activation='relu', name='dense2')(dense1)
+    dropout1 = Dropout(0.1, name='dropout1')(dense2)
+    output = Dense(CLASS_COUNT, activation=activations, name='output')(dropout1)
+    model = Model(inputs=[ps_nodes_input, emb_input], outputs=output)
     optimiser = adam(lr=learning_rate)
     model.compile(loss='mean_squared_error',
                   optimizer=optimiser,
