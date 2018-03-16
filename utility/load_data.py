da049c403a57a257af3c8ed119a57684754f287d
tanhanliang <tanhanliang@github.com>
2018-03-16 11:38:24 +0000

    Updated load data, renamed training data files

--- utility/load_data.py
+++ utility/load_data.py
@@ -8,17 +8,18 @@ def load_data():
     """
     Loads data for the latest version of the model.
 
-    :return: A tuple of ndarrays, (x,y).
-    x has shape (training_examples, field_size*field_count, channel_count, 1)
-    y has shape (training_examples, class_count)
+    :return: A tuple of (list of inputs, target values). The inputs and target values
+    are numpy ndarrays
     """
 
-    x_embed = np.fromfile("training_data/x_embed4.txt")
-    x_patchy = np.fromfile("training_data/x_patchy4.txt")
-    y = np.fromfile("training_data/y_train4.txt")
-    # Shape is currently hardcoded until i find a better way to do it
-    x_embed = x_embed.reshape((2000, 40))
-    x_patchy = x_patchy.reshape((2000, 4, 5, 1))
+    xpn = np.fromfile("training_data/x_patchy_nodes6.txt")
+    xpe = np.fromfile("training_data/x_patchy_edges6.txt")
+    xe = np.fromfile("training_data/x_embed6.txt")
+    y = np.fromfile("training_data/y_train6.txt")
+
+    xpn = xpn.reshape((2000, 6, 5, 1))
+    xpe = xpe.reshape((2000, 36, 2, 1))
+    xe = xe.reshape((2000, 120))
     y = y.reshape((2000, 2))
 
-    return x_patchy, x_embed, y
+    return [xpn, xpe, xe], y
