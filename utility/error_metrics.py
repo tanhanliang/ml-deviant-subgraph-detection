a415afb52edb1b283b125ded5ada867c4afe5e33
tanhanliang <tanhanliang@github.com>
2018-05-15 23:54:54 +0100

    Error bounds and precision/recall

--- utility/error_metrics.py
+++ utility/error_metrics.py
@@ -8,7 +8,7 @@ from sklearn.metrics import classification_report
 Z_95 = 1.96
 
 
-def std_dev(x, y, model):
+def variance(x, y, model):
     """
     Computes the estimated standard deviation using Bessel's correction.
 
@@ -24,20 +24,20 @@ def std_dev(x, y, model):
 
     # Accuracy
     mean = model.evaluate(x, y)[1]
-    std = 0.0
+    var = 0.0
     y_classes = [np.argmax(y_elem, axis=None, out=None) for y_elem in y]
 
     for idx in range(training_examples):
         if y_classes[idx] == pred_labels[idx]:
-            std += (mean - 1).__pow__(2)
+            var += (mean - 1).__pow__(2)
         else:
-            std += (mean).__pow__(2)
+            var += (mean).__pow__(2)
 
-    std = (1/(training_examples-1))*std
-    return std
+    var = (1/(training_examples-1))*var
+    return var
 
 
-def print_error_bound(x, y, model):
+def get_error_bound(x, y, model):
     """
     Computes and prints the error bound within a certain confidence interval for the accuracy of the model.
 
@@ -47,22 +47,25 @@ def print_error_bound(x, y, model):
     :return: Nothing
     """
 
-    std = std_dev(x, y, model)
+    var = variance(x, y, model)
     training_examples = len(y)
-    print(Z_95*sqrt(pow(std, 2)/training_examples))
+    bound = Z_95*sqrt(var/training_examples)
+    print(bound)
+    return bound
 
 
-def print_precision_recall(x, y, model):
+def get_precision_recall(x, y, model):
     """
     Prints the classification report.
 
     :param x: A list of inputs to the model.
     :param y: The target labels. A ndarray.
     :param model: The model in question.
-    :return: nothing
+    :return: the classification report, a string
     """
 
     pred = model.predict(x)
     predicted = np.argmax(pred, axis=-1)
     report = classification_report(np.argmax(y, axis=-1), predicted, digits=4)
     print(report)
+    return report
